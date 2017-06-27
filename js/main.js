6ba42c2023a45d4a8151d3914aa7e54fcafcb0c0
Yasmin Aljedawi <yaljedawi@gmail.com>
2017-06-27 17:50:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
-      // 1. GGGI for all countries - 2013
+      // 1. GGGI for all countries - 2013 Graph
+      /*
+        D3.js setup code
+      */
       var svg = dimple.newSvg("#chart1", 1000, 1900);
-
+      // append a title to the graph
       svg.append("text")
       .attr("x", (svg[0][0].clientWidth / 2))
       .attr("y", 150)
@@ -9,8 +12,14 @@
       .style("font-size", "20px")
       .style("font-weight", "bold")
       .text("Global Gender Gap Index (GGGI) Scores - 2013");
-
+    /*
+    Use D3 (not dimple.js) to load the CSV file
+    and pass the contents of it to a charting function
+    */
       d3.csv("data/ggi_2013_score_all.csv", function (data) {
+            /*
+            Dimple.js Chart construction code
+            */
       	var myChart = new dimple.chart(svg, data);
       	myChart.addMeasureAxis("x", "2013 Score");
       	myChart.addCategoryAxis("y", "Country");
@@ -19,10 +28,13 @@
       });
 
 
-      // 2. Top 10 countries scores 2008, 2013
+      // 2. Top 10 countries scores 2008, 2013 Graph
+      /*
+        D3.js setup code
+      */
       var width = 1000, height = 600;
       var svg2 = dimple.newSvg("#chart2", width, height);
-
+      // append a title to the graph
       svg2.append("text")
       .attr("x", (svg2[0][0].clientWidth / 2))
       .attr("y", 30)
@@ -30,8 +42,14 @@
       .style("font-size", "20px")
       .style("font-weight", "bold")
       .text("Top 10 Countries GGGI Scores for Years 2008, 2013");
-
+    /*
+    Use D3 (not dimple.js) to load the CSV file
+    and pass the contents of it to a charting function
+    */
       d3.csv("data/top_10_countries_08-13.csv", function draw(data) {
+            /*
+            Dimple.js Chart construction code
+            */
       	var myChart = new dimple.chart(svg2, data);
       	myChart.addCategoryAxis("x", ["Country", "variable"]); 
       	myChart.addMeasureAxis("y", "value");
@@ -41,9 +59,12 @@
       });
 
 
-      // 3. Arab countries performance 2007-2013
+      // 3. Arab countries performance 2007-2013 Graph
+      /*
+        D3.js setup code
+      */
       var svg3 = dimple.newSvg("#chart3", width, height);
-
+      // append a title to the graph
       svg3.append("text")
       .attr("x", (svg3[0][0].clientWidth / 2))
       .attr("y", 30)
@@ -51,8 +72,14 @@
       .style("font-size", "20px")
       .style("font-weight", "bold")
       .text("GGGI for Arab countries 2007 - 2013");
-
+    /*
+    Use D3 (not dimple.js) to load the CSV file
+    and pass the contents of it to a charting function
+    */
       d3.csv("data/ggi_arab_scores_08-13.csv", function draw(data) {
+            /*
+            Dimple.js Chart construction code
+            */
       	var myChart = new dimple.chart(svg3, data);
       	myChart.addCategoryAxis("x", "variable"); 
       	y = myChart.addMeasureAxis("y", "value");
